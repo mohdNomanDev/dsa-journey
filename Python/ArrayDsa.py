957ee4a2242f99eb27c86771ab889681f6afcc1d
mohdNomanDev <mohdnoman0302@gmail.com>
2026-09-15 03:07:26 +0530

    reverse_Arry

--- Python/ArrayDsa.py
+++ Python/ArrayDsa.py
@@ -21,9 +21,17 @@ def binary_search(target,arr):
         else: e = m - 1
     return -1
         
+def reverse_Arry(arr):
+    for left in range(len(arr)):
+        if left > (len(arr)/2)-1:
+            break
+        temp = arr[len(arr)-1-left]
+        arr[len(arr)-1-left] = arr[left]
+        arr[left] = temp
 
-arr_list = [10, 20, 30, 40, 50, 60, 70]
 
-index = binary_search(65,arr_list)
+arr_list = [10, 20, 30, 40, 50, 60]
 
-print(index)
+# index = binary_search(65,arr_list)
+reverse_Arry(arr_list)
+print(arr_list)
