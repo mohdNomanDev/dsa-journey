b2d9ef38b3067f7aa12832e035494c05d70f6480
mohdNomanDev <mohdnoman0302@gmail.com>
2026-09-15 00:33:14 +0530

    array

--- Java Dsa/ArrayDsa.java
+++ Java Dsa/ArrayDsa.java
@@ -1,4 +1,4 @@
-public class LinearSearch {
+public class ArrayDsa {
 
     public static int linearSearch(int search, int[] nums){
         for (int i = 0; i < nums.length; i++) {
@@ -8,6 +8,8 @@ public class LinearSearch {
         }
         return -1;
     }
+
+    
     public static void main(String[] args){
         int[] arr = {5, 12, 8, 20, 3, 15};
         int index = linearSearch(20, arr);
