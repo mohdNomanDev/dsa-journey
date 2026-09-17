b24e6013e24b3629cd7965c1d39e566322c07c1f
mohdNomanDev <mohdnoman0302@gmail.com>
2026-09-17 12:28:35 +0530

    maxSubArray

--- Java Dsa/ArrayDsa.java
+++ Java Dsa/ArrayDsa.java
@@ -68,12 +68,33 @@ public class ArrayDsa {
             System.out.println();
         }
     }
+
+    public static void maxSubArray(int[] arr){
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            
+            for (int j = i; j < arr.length; j++) {
+                int sum = 0;
+                System.out.print('(');
+                for (int k = i; k <= j; k++){
+                    System.out.print(arr[k]);
+                    System.out.print(',');
+                    sum += arr[k];
+                }
+                max = Math.max(max, sum);
+                System.out.print(") = "+sum+"\t");
+                
+            }
+            System.out.println();
+            System.out.println("Maximum : "+max);
+        }
+    }
     
     public static void main(String[] args){
-        int[] arr = {10, 20, 30, 40, 50, 60};
+        int[] arr = {2, -1, 3, -4, 5};
         // int index = binarySearch( 90, arr);
         // System.out.println(index);
         
-        subArray(arr);
+        maxSubArray(arr);
     }
 }
