44d308a1a126432eb2e7e21a272459016e2e4473
mohdNomanDev <mohdnoman0302@gmail.com>
2026-09-21 12:52:39 +0530

    bubbleSort

--- Java Dsa/ArrayDsa.java
+++ Java Dsa/ArrayDsa.java
@@ -104,10 +104,28 @@ public class ArrayDsa {
 
         System.out.println("Total water trapped: "+sum);
     }
+
+    public static void bubbleSort(int[] arr){
+        int len = arr.length;
+        for(int i = 0; i < len - 1; i ++){
+           for (int j = 0; j < len - 1 - i; j++) {
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+           }
+
+        }
+    }
     
     public static void main(String[] args){
-        int[] arr = {4, 2, 0, 3, 2, 5};
+        int[] arr = {9,8,7,6,5};
         
-        trapWater(arr);
+        bubbleSort(arr);
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
     }
 }
